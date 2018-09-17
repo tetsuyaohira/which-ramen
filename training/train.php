8847438c6d57da3589bbee433f60b0d8c83ec4f4
tetsuya <tetsuya.ohira@icloud.com>
2018-09-17 12:06:58 +0900

    update

---
+++ training/train.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tetsuya
+ * Date: 2018/09/17
+ * Time: 12:06
+ */
---
+++ userInterface/ramen-ui.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'histogram-lib.inc.php';
+$up_form = <<<EOS
+
+EOS;
+
+$head = '';
+$foot = '';
