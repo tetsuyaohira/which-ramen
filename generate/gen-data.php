4084b629b8dc633f39ca6f439b6fc98139fec898
tetsuya <tetsuya.ohira@icloud.com>
2018-09-17 11:57:43 +0900

    update

--- generate/gen-data.php
+++ generate/gen-data.php
@@ -8,13 +8,40 @@ $ramen_type = [
 ];
 
 
-gen_data('',40);
-gen_data('-test',14);
+gen_data('', 40);
+gen_data('-test', 14);
 
 echo "ok/n";
 
 
+function gen_data($dir_type, $count)
+{
 
-function gen_data($dir_type,$count) {
+    $sio_list = glob("sio{$dir_type}/*.jpg");
+    $miso_list = glob("miso{$dir_type}/*.jpg");
 
+    shuffle($sio_list);
+    shuffle($miso_list);
+    $sio_list = array_slice($sio_list, 0, $count);
+    $miso_list = array_slice($miso_list, 0, $count);
+
+    $count = count($sio_list) + count($miso_list);
+    $data = "$count 64 2\n";
+    $data .= gen_fann_data($sio_list, 'sio');
+    $data .= gen_fann_data($miso_list, 'miso');
+    file_put_contents("ramen{$dir_type}.dat", $data);
+}
+
+function gen_fann_data($list, $type)
+{
+    global $ramen_type;
+    $out = $ramen_type;
+    $data = '';
+    foreach ($list as $f) {
+        $his = make_histogram($f);
+        $data .= implode(' ', $his) . "\n";
+        $data .= $out . "\n";
+
+    }
+    return $data;
 }
